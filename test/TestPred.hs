6606c42852d0f9f623e13a85be07fcc5e0edcb03
Victor Alejandro Díaz Jáuregui <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-22 23:32:36 -0300

    Avance del TestPred

--- test/TestPred.hs
+++ test/TestPred.hs
@@ -1,9 +1,46 @@
-module Main (main) where
-import System.Exit (exitFailure)
-import Control.Monad (when)
-import Pred (falla)
+module Main (
+    main
+) where
+import Test.HUnit
+import Pred (Pred, cambiar, anyFig, allFig)
+import Dibujo (Dibujo, figura)
+
+-- Tests para 'cambiar'
+testCambiar = TestCase $ assertEqual "Cambiar" expected result
+    where
+        predicado :: Pred String
+        predicado = (== "a")
+        transformacion :: String -> Dibujo String
+        transformacion = figura
+        dibujo = figura "a"
+        expected = transformacion "a"
+        result = cambiar predicado transformacion dibujo
+
+-- Tests para 'anyFig'
+testAnyFig = TestCase $ assertEqual "AnyFig" expected result
+    where
+        predicado :: Pred String
+        predicado = (== "a")
+        dibujo = figura "a"
+        expected = True
+        result = anyFig predicado dibujo
+
+-- Tests para 'allFig'
+testAllFig = TestCase $ assertEqual "AllFig" expected result
+    where
+        predicado :: Pred String
+        predicado = (== "a")
+        dibujo = figura "a"
+        expected = True
+        result = allFig predicado dibujo
+
+tests = TestList [
+     testCambiar,
+     testAnyFig,
+     testAllFig
+    ]
 
 main :: IO ()
 main = do
-    putStrLn "Test suite not yet implemented."
-    when falla exitFailure
+    _ <- runTestTT tests
+    return ()
